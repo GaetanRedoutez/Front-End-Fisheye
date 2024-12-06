a141392c9e813d89ff4b858b6edfcf52a169dd54
Gaetan Redoutez <gaetan.redoutez@preconys.com>
2024-12-06 15:29:14 +0100

    feat(contact): start include dialogbox

---
+++ src/components/PhotographPage/DialogBox/index.jsx
@@ -0,0 +1,26 @@
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './index.css';
+
+const DialogBox = ({ isOpen, onClose, photograph }) => {
+	if (!isOpen) return null;
+
+	return (
+		<div className="dialog-overlay">
+			<div className="dialog-box">
+				<div className="dialog-header">
+					<h2 className="dialog-title">
+						<div>Contactez-moi</div>
+						<div>{photograph.name}</div>
+					</h2>
+					<div className="dialog-close">
+						<FontAwesomeIcon icon={faX} onClick={onClose} />
+					</div>
+				</div>
+				<div>Formulaire de contact</div>
+			</div>
+		</div>
+	);
+};
+
+export default DialogBox;
